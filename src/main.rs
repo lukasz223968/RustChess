c00de5cd9036f6b67f9f843fb2d8d3d28a3ba90a
lukasz223968 <223968@edu.p.lodz.pl>
2021-10-14 10:25:20 +0200

    minor changes

--- src/main.rs
+++ src/main.rs
@@ -37,24 +37,24 @@ fn main() {
     // println!("moves: {:#?}", board.generate_legal_moves(init::BB_ALL, init::BB_ALL).collect::<Vec<Move>>());
     use pgn::Node;
     let mut a = Node::new("hey");
-    let mut b = a.add_variation(Move::from_uci("D2D4"), "comment", "hehe", HashSet::new());
-    let mut c = b.add_variation(Move::from_uci("D7D5"), "comment2", "hehe2",HashSet::new());
+    let mut b = a.add_variation(Move::from_uci("D2D4"), "comment", "starting", HashSet::new());
+    let mut c = b.add_variation(Move::from_uci("D7D5"), "comment2", "starting2",HashSet::new());
     c = c.add_variation(Move::from_uci("C2C4"), "fdsa", "hdsa",HashSet::new());
     let board = c.board();
     println!("{}", board.baseboard.unicode(board.turn, false, "."));
 }
 
-extern crate test;
-use test::Bencher;
+// extern crate test;
+// use test::Bencher;
 
-#[bench]
-fn bench_cp(b :&mut Bencher) {
-    test::black_box(
-         b.iter( || {
-            let a = crate::pgn::Node::new("hey");
-             (0..10000000).map(move |x| {
-                 &a.0;
-             })
-         })
-    )
-}
+// #[bench]
+// fn bench_cp(b :&mut Bencher) {
+//     test::black_box(
+//          b.iter( || {
+//             let a = crate::pgn::Node::new("hey");
+//              (0..10000000).map(move |x| {
+//                  &a.0;
+//              })
+//          })
+//     )
+// }
